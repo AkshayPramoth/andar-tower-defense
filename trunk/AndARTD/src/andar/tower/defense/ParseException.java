93af4ba2ec4edd40378533c37b4e13808d044ab1
jakob.klein.freelancer <jakob.klein.freelancer@493191f4-5c3c-d4d5-740c-e345a6c1b7bb>
2011-01-06 13:08:45 +0000

    Initial import

---
+++ trunk/AndARTD/src/andar/tower/defense/ParseException.java
@@ -0,0 +1,10 @@
+package andar.tower.defense;
+
+
+
+
+public class ParseException extends Exception {
+	public ParseException(String file,int lineNumber, String msg) {
+		super("Parse error in file "+file+"on line "+lineNumber+":"+msg);
+	}
+}
