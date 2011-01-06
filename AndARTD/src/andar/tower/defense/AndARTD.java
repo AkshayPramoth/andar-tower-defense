f1050ad7271aa13df89d60cf47b1e9ee063971f9
jakob.klein.freelancer <jakob.klein.freelancer@493191f4-5c3c-d4d5-740c-e345a6c1b7bb>
2011-01-06 13:08:45 +0000

    Initial import

---
+++ AndARTD/src/andar/tower/defense/AndARTD.java
@@ -0,0 +1,13 @@
+package andar.tower.defense;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AndARTD extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
