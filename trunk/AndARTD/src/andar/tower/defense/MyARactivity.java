03174245764b0a488196d188b7dd22f3cf0295a9
jakob.klein.freelancer <jakob.klein.freelancer@493191f4-5c3c-d4d5-740c-e345a6c1b7bb>
2011-01-08 11:50:00 +0000

    load many models

--- trunk/AndARTD/src/andar/tower/defense/MyARactivity.java
+++ trunk/AndARTD/src/andar/tower/defense/MyARactivity.java
@@ -10,6 +10,7 @@ import android.net.ParseException;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Debug;
+import android.util.Log;
 import android.view.SurfaceHolder;
 import edu.dhbw.andar.ARToolkit;
 import edu.dhbw.andar.AndARActivity;
@@ -22,7 +23,15 @@ public class MyARactivity extends AndARActivity implements SurfaceHolder.Callbac
 	
 
 	private Model model;
+	private Model model2;
+	private Model model3;
+	private Model model4;
+	private Model model5;
 	private Model3D model3d;
+	private Model3D model3d2;
+	private Model3D model3d3;
+	private Model3D model3d4;
+	private Model3D model3d5;
 	private ProgressDialog waitDialog;
 	private Resources res;
 	public static final boolean DEBUG = false;
@@ -71,6 +80,23 @@ public class MyARactivity extends AndARActivity implements SurfaceHolder.Callbac
 	    
 		private class ModelLoader extends AsyncTask<Void, Void, Void> {
 			
+			private String modelName2patternName (String modelName) {
+				String patternName = "android";
+				
+				if (modelName.equals("plant.obj")) {
+					patternName = "marker_rupee16";
+				} else if (modelName.equals("chair.obj")) {
+					patternName = "marker_fisch16";
+				} else if (modelName.equals("tower.obj")) {
+					patternName = "marker_peace16";
+				} else if (modelName.equals("bench.obj")) {
+					patternName = "marker_at16";
+				} else if (modelName.equals("towergreen.obj")) {
+					patternName = "marker_hand16";
+				}
+				
+				return patternName;
+			}
 			
 	    	@Override
 	    	protected Void doInBackground(Void... params) {
@@ -92,10 +118,48 @@ public class MyARactivity extends AndARActivity implements SurfaceHolder.Callbac
 							Debug.startMethodTracing("AndObjViewer");
 				
 						if(fileUtil != null) {
+
 							BufferedReader fileReader = fileUtil.getReaderFromName(modelFileName);
 							if(fileReader != null) {
 								model = parser.parse("Model", fileReader);
-								model3d = new Model3D(model);
+								Log.w("ModelLoader", "model3d = new Model3D(model, " + modelName2patternName(modelFileName) + ".patt");
+								model3d = new Model3D(model, modelName2patternName(modelFileName) + ".patt");
+							}
+							String modelFileName2 = "chair.obj";
+							BufferedReader fileReader2 = fileUtil.getReaderFromName(modelFileName2);
+							if(fileReader2 != null) {
+								model2 = parser.parse("Chair", fileReader2);
+								Log.w("ModelLoader", "model3d = new Model3D(model2, " + modelName2patternName(modelFileName2) + ".patt");
+								model3d2 = new Model3D(model2, modelName2patternName(modelFileName2) + ".patt");
+							} else {
+								Log.w("ModelLoader", "no file reader");
+							}
+							String modelFileName3 = "towergreen.obj";
+							BufferedReader fileReader3 = fileUtil.getReaderFromName(modelFileName3);
+							if(fileReader3 != null) {
+								model3 = parser.parse("towergreen", fileReader3);
+								Log.w("ModelLoader", "model3d = new Model3D(model3, " + modelName2patternName(modelFileName3) + ".patt");
+								model3d3 = new Model3D(model3, modelName2patternName(modelFileName3) + ".patt");
+							} else {
+								Log.w("ModelLoader", "no file reader");
+							}
+							String modelFileName4 = "tower.obj";
+							BufferedReader fileReader4 = fileUtil.getReaderFromName(modelFileName4);
+							if(fileReader4 != null) {
+								model4 = parser.parse("tower", fileReader4);
+								Log.w("ModelLoader", "model3d = new Model3D(model4, " + modelName2patternName(modelFileName4) + ".patt");
+								model3d4 = new Model3D(model4, modelName2patternName(modelFileName4) + ".patt");
+							} else {
+								Log.w("ModelLoader", "no file reader");
+							}
+							String modelFileName5 = "plant.obj";
+							BufferedReader fileReader5 = fileUtil.getReaderFromName(modelFileName5);
+							if(fileReader5 != null) {
+								model5 = parser.parse("Plant", fileReader5);
+								Log.w("ModelLoader", "model3d = new Model3D(model5, " + modelName2patternName(modelFileName5) + ".patt");
+								model3d5 = new Model3D(model5, modelName2patternName(modelFileName5) + ".patt");
+							} else {
+								Log.w("ModelLoader", "no file reader");
 							}
 						}
 						if(Config.DEBUG)
